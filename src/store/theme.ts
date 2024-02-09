0c12038c2aeaae21e4e89ac07b8e1b05b462ebe2
Mikkel RINGAUD <mikkel@milescode.dev>
2024-02-09 16:50:21 +0100

    fix: use createRoot for store computations

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,18 +1,26 @@
 import { createPrefersDark } from "@solid-primitives/media";
-import { createSignal, createMemo } from "solid-js";
+import { createSignal, createMemo, createRoot } from "solid-js";
 
-const prefersDark = createPrefersDark();
 export enum THEME_TYPE {
   SYSTEM = "system",
   LIGHT = "light",
   DARK = "dark",
 }
 
-export const [selectedTheme, setSelectedTheme] = createSignal<THEME_TYPE>(THEME_TYPE.SYSTEM);
-export const shouldUseDarkTheme = createMemo(() => {
-  if (selectedTheme() === THEME_TYPE.SYSTEM) {
-    return prefersDark();
-  }
+export const { selectedTheme, setSelectedTheme, shouldUseDarkTheme} = createRoot(() => {
+  const prefersDark = createPrefersDark();
+  const [selectedTheme, setSelectedTheme] = createSignal<THEME_TYPE>(THEME_TYPE.SYSTEM);
+  const shouldUseDarkTheme = createMemo(() => {
+    if (selectedTheme() === THEME_TYPE.SYSTEM) {
+      return prefersDark();
+    }
+  
+    return selectedTheme() === THEME_TYPE.DARK;
+  });
 
-  return selectedTheme() === THEME_TYPE.DARK;
+  return {
+    selectedTheme,
+    setSelectedTheme,
+    shouldUseDarkTheme,
+  };
 });
