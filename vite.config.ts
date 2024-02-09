698566b41f71d69233f7b7d8fddd64d3a7365e3a
Mikkel RINGAUD <mikkel@milescode.dev>
2024-02-09 11:58:34 +0100

    chore(deps): add icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "@solidjs/start/config";
+import icons from "unplugin-icons/vite";
 
 export default defineConfig({
+  plugins: [icons({ compiler: "solid" })],
   start: { ssr: false }
 });
